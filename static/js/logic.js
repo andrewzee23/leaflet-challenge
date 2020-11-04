7d6f26a24e4abb0749ddacaa552c34e59622c1f0
andrewzee23 <andrewazamora@gmail.com>
2020-11-04 14:00:35 -0600

    adding d3

--- static/js/logic.js
+++ static/js/logic.js
@@ -20,3 +20,9 @@ function markerColor(mag) {
       return "#FF0000";
   };
 }
+// Perform a GET request to the query URL
+d3.json(link, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+});
+
