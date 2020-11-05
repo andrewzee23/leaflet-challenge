838325fbbf99f8bdbad7d81c6cad31f70ae2da8e
andrewzee23 <andrewazamora@gmail.com>
2020-11-05 13:45:45 -0600

    formatting

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiYW5kcmV3emVlIiwiYSI6ImNrZ251YW85aTBqNjcydnF0MzE2ZmtnMnEifQ.rdmKLDOpGiSDPXmnmNkyUg";
+const API_KEY = "pk.eyJ1IjoiYW5kcmV3emVlIiwiYSI6ImNraDUweGNmcjA2bnIycW10ZWRrZHptYTUifQ.iMBUmLOZG0rxBVzAZcmemQ";
